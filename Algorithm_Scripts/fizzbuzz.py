b770d01271494d910e216ed06a79dd06075b92cd
osherboudara99 <osherboudara99@gmail.com>
2025-07-07 11:21:31 -0700

    added single numbher algorithm

--- Algorithm_Scripts/fizzbuzz.py
+++ Algorithm_Scripts/fizzbuzz.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/problems/fizz-buzz/description/
 # Given an integer n, return a string array answer (1-indexed) where:
 
 # answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
