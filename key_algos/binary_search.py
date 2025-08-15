5d325c06b76eda2f6789cee7bdfe11f63a500cfa
osherboudara99 <osherboudara99@gmail.com>
2025-08-15 15:52:46 -0400

    binary search notes added

---
+++ key_algos/binary_search.py
@@ -0,0 +1,50 @@
+def binary_search(l:list, val :int) -> int:
+    
+    low = 0 
+    high = len(l)-1
+
+
+    while low <= high:
+
+        mid = (low + high) // 2 
+
+        if l[mid] == val:
+            return mid 
+    
+        elif l[mid] < val:
+            low = mid + 1 
+        
+        else:
+            high = mid - 1 
+    return -1 
+
+l = [1,2,3,4,5,6,7,8]
+print(binary_search(l, 8))
+
+
+def binary_search(l:list, low:int, high:int, val:int) -> int:
+
+    if low <= high:
+
+        mid = (low + high) // 2 
+
+        if l[mid] == val:
+            return mid 
+        
+        elif l[mid]< val:
+            return binary_search(l, mid+1, high, val)
+
+        else:
+            return binary_search(l, low, mid-1, val)
+    
+    return -1
+
+l = [1,2,3,4,5,6,7,8]
+print(binary_search(l, 0, len(l), 8))
+
+
+
+
+
+    
+            
